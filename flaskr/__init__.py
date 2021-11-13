b1e7dcc62652f07840225eddf7384741f7e8de95
Ofek Peres <operes@princeton.edu>
2021-11-12 21:08:56 -0500

    working version of rrt

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,14 @@
 # A bit of a circular import, but because our app is starting off simple:
 # First, create the app using the 2 lines below
 from flask import Flask
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 # Import all of the python files we want from our directory name
 # These files can in turn import app from the __init__.py file
 # and therefore add routes and methods as we want
+# from flaskr import rrt
 from flaskr import views, random, echo, rrt
+
+if  __name__ == "__main__":
+    app.run(debug=True)
--- flaskr/rrt.py
+++ flaskr/rrt.py
@@ -1,8 +1,8 @@
 from flask import jsonify, request, make_response
 from flaskr import app
-from .submodules.RRT.src.RRT import RRT
 import json
 import numpy as np
+from .submodules.RRT.src.RRT import RRT
 
 class NumpyEncoder(json.JSONEncoder):
     """ Special json encoder for numpy types """
@@ -17,19 +17,44 @@ class NumpyEncoder(json.JSONEncoder):
 
 
 # http://localhost:5000/random?start=5&end=20
-@app.route('/rrt')
+@app.route('/rrt', methods=["GET", "POST"])
 def get_rrt():
-    payload = {}
-    payload['obstacles'] = [{"shape":"rectangle", "definition":[20,10,40,20]},{"shape":"circle", "definition":[10,10,3]}, {"shape":"circle", "definition":[50,50,20]}]
-    payload['start'] = [0,0]
-    payload['goal'] = [90,25]
-    payload['goalRadius'] = 10
-    payload['d_max'] = 28
-    payload['width'] = 400
-    payload['height'] = 400
-    rrt = RRT(payload)
-    
-    response = make_response(json.dumps(rrt.GetRRTPayload(), cls=NumpyEncoder))
-    response.content_type = 'application/json'
-    return response
+    if request.method == "GET":
+        payload = {}
+        payload['obstacles'] = [{"shape":"rectangle", "definition":[20,10,40,20]},{"shape":"circle", "definition":[10,10,3]}, {"shape":"circle", "definition":[50,50,20]}]
+        payload['start'] = [0,0]
+        payload['goal'] = [90,25]
+        payload['goalRadius'] = 10
+        payload['d_max'] = 28
+        payload['width'] = 400
+        payload['height'] = 400
+        rrt = RRT(payload)
+        
+        response = make_response(json.dumps(rrt.GetRRTPayload(), cls=NumpyEncoder))
+        response.content_type = 'application/json'
+        return response
+    if request.method == "POST":
+        payload = request.get_json()
+        # print("----------------------------")
+        # print("Printing Post method output")
+        # print("Start: {}".format(payload['start']))
+        # print("Goal: {}".format(payload['goal']))
+        # print("Goal Radius: {}".format(payload['goalRadius']))
+        # print("Goal Radius type: {}".format(type(payload['goalRadius'])))
+        # print("d_max: {}".format(payload['d_max']))
+        # print("Width: {}".format(payload['width']))
+        # print("Height: {}".format(payload['height']))
+        # print("Obstacles: {}".format(payload['obstacles']))
+        # print("----------------------------")
+
+        
+        # print("Type of goalRadius: {}".format(type(payload['goalRadius'])))
+        # payload['goalRadius'] = float(payload['goalRadius'])
+        # print("Type of goalRadius: {}".format(type(payload['goalRadius'])))
+        
+        rrt = RRT(payload)
+        
+        response = make_response(json.dumps(rrt.GetRRTPayload(), cls=NumpyEncoder))
+        response.content_type = 'application/json'
+        return response
 
