ae70d8da2556fd31c2280f7645bfefba8d9332c6
Ofek Peres <operes@princeton.edu>
2021-11-14 11:05:50 -0500

    updated echo to demo get and post to div

--- flaskr/echo.py
+++ flaskr/echo.py
@@ -1,7 +1,15 @@
+from werkzeug.wrappers import request
 from flaskr import app
-from flask import jsonify
+from flask import jsonify, request, make_response
 from markupsafe import escape
 @app.route('/echo/<text>', methods=['GET', 'POST'])
 def show_user_profile(text):
     # show the user profile for that user
-    return jsonify(f'{escape(text)}')
+
+    if request.method == "GET":
+        return jsonify(f'{escape(text)}')
+    
+    elif request.method == "POST":
+        print("In post request")
+        payload = request.get_json()
+        return jsonify(payload)
